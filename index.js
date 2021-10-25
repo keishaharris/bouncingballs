73702d279d023e5b065b4eefb18ac8e6b4fd27a3
Keisha Harris <keishadharriss@gmail.com>
2021-10-24 23:37:04 -0400

    refactor code - created a Ball object and added multiple balls

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById("myCanvas");
+/* const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
 canvas.style.background = "#C7F6AA";
@@ -17,11 +17,7 @@ let r = 40;
 const velocity = 10;
 
 function colorBall(){
-    let color = '#';
-    let letters = '0123456789ABCDEF'
-    for (let i = 0; i < 6; i++) {
-        color += letters[(Math.floor(Math.randon) * 16)]; 
-    }
+    let color = "rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256)+ ")";
     return color;
 }
 
@@ -61,3 +57,87 @@ function draw(){
 
 setInterval(
     draw, 10);
+    */
+    
+
+    const canvas = document.getElementById("myCanvas");
+    const ctx = canvas.getContext("2d");
+    let width = canvas.width = window.innerWidth;
+    let height = canvas.height = window.innerHeight;
+
+    canvas.style.background = "#C7F6AA";
+
+    //creating Ball object 
+    function Ball(x, y, r, speedx, speedy, color){
+
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.color = color;
+        //"rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256)+ ")";
+        this.speedx = speedx;
+        this.speedy = speedy;
+    }
+
+          //Draw Ball
+    Ball.prototype.drawBalls = function() {
+        
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x,this.y,this.r,0,2 * Math.PI);
+        ctx.fill();
+    }    
+
+    Ball.prototype.movePosition = function(){
+        if((this.x + this.r) >= width || (this.x - this.r) <=0) {
+            this.speedx = -this.speedx;
+        }
+        if((this.y + this.r) >= height || (this.y - this.r) <= 0){
+            this.speedy = -this.speedy;
+        }    
+
+        this.x += this.speedx;
+        this.y += this.speedy;
+
+        }
+
+
+    function rand (min, max) {
+        const num =  Math.floor(Math.random() * (max - min + 1)) + min; 
+        return num;
+    }
+
+    const balls = [];
+    
+    while(balls.length <35){
+
+        //create new instance of Ball 
+        let halfx = width/2;
+        let halfy = height/2;
+
+        let ball = new Ball(
+            halfx, 
+            halfy, 
+            rand(20,50), 
+            rand(-10,10), rand(-10, 10), 
+            'rgb(' + rand(0,255) + ',' + rand(0,255) + ',' + rand(0,255) +  ',' + (Math.ceil(Math.random() * 10) / 10) + ')',
+            );
+        balls.push(ball);
+    }
+    
+
+    function loop(){
+
+        ctx.clearRect(0,0, width, height);
+
+
+        for(let i = 0; i < balls.length; i++){
+            balls[i].drawBalls();
+            balls[i].movePosition();
+        }
+
+        //calls loop function repeatedly 
+        window.requestAnimationFrame(loop);
+
+    }
+loop(); 
